a521b4403c292aeb06991f8040782c9876418de2
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-04-07 17:47:47 +0600

    delete api create

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,12 +3,25 @@ import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
     const users = useLoaderData();
+
+    const handleDeleteUser = (_id) =>{
+        console.log('deleted id', _id);
+        
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
     return (
         <div>
             <h1>{users.length}</h1>
             <div>
                 {
-                    users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                    users.map(user => <p key={user._id}>{user.name} : {user.email} : {user._id}
+                    <button onClick={() => handleDeleteUser(user._id)}>x</button>
+                    </p>)
                 }
             </div>
         </div>
